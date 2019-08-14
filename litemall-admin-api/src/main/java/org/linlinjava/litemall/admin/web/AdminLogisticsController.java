7bb50107f807d836b0dc5933fee1951276e8de11
lizemin <lizemin@mobike.com>
2019-08-14 14:57:40 +0800

    add interface

--- litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/web/AdminLogisticsController.java
+++ litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/web/AdminLogisticsController.java
@@ -2,9 +2,15 @@ package org.linlinjava.litemall.admin.web;
 
 /*物流管理*/
 
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.linlinjava.litemall.admin.annotation.RequiresPermissionsDesc;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/admin/logistics")
@@ -12,33 +18,144 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminLogisticsController {
 
     /*公司管理更新、新增*/
-
-    public Object addLogisticsCompany(){
+    @RequiresPermissions("admin:logistics:add")
+    @RequiresPermissionsDesc(menu = {"物流管理","物流公司管理"},button = "新增")
+    @RequestMapping("/addCompany")
+    @ResponseBody
+    public Object addLogisticsCompany(HttpServletRequest request,
+                                      @RequestParam(value = "name") String name,
+                                      @RequestParam(value = "type") int type,
+                                      @RequestParam(value = "address") String address,
+                                      @RequestParam(value = "phone") String phone,
+                                      @RequestParam(value = "contact") String contact,
+                                      @RequestParam(value = "serviceTel",required = false) String serviceTel){
         return null;
     }
 
-    public Object updateLogisticsCompany(){
+    @RequiresPermissions("admin:logistics:update")
+    @RequiresPermissionsDesc(menu = {"物流管理","物流公司管理"},button = "修改")
+    @RequestMapping("/updateCompany")
+    @ResponseBody
+    public Object updateLogisticsCompany(HttpServletRequest request,
+                                         @RequestParam(value = "name") String name,
+                                         @RequestParam(value = "type") int type,
+                                         @RequestParam(value = "address") String address,
+                                         @RequestParam(value = "phone") String phone,
+                                         @RequestParam(value = "contact") String contact,
+                                         @RequestParam(value = "serviceTel",required = false) String serviceTel,
+                                         @RequestParam(value = "id") long id){
         return null;
     }
+
+    /*当id为空时，返回全部列表，否则返回指定的id详情*/
+    @RequiresPermissions("admin:logistics:list")
+    @RequiresPermissionsDesc(menu ={"物流管理","物流公司管理"},button = "详情")
+    @RequestMapping("/listCompany")
+    @ResponseBody
+    public Object logisticsCompanyList(HttpServletRequest request,
+                                       @RequestParam(value = "id", required = false) long id){
+        return null;
+    }
+
 
     /*车辆管理 更新 新增 逻辑删除*/
-
-    public Object addTruck(){
+    @RequiresPermissions("admin:logistics:addtruck")
+    @RequiresPermissionsDesc(menu ={"物流管理","车辆管理"},button = "新增")
+    @RequestMapping("/addTruck")
+    @ResponseBody
+    public Object addTruck(HttpServletRequest request,
+                           @RequestParam(value = "companyId") long comanpyId,
+                           @RequestParam(value = "licensePlateNumber") String licensePlateNumber,
+                           @RequestParam(value = "driver") String driver,
+                           @RequestParam(value = "phone") String phone,
+                           @RequestParam(value = "load") String load
+                           ){
         return null;
     }
 
-    public Object updateTruck(){
+    /*修改车牌号时需检测最近一个月定单是否有物流配送，否则不允许修改*/
+    @RequiresPermissions("admin:logistics:updatetruck")
+    @RequiresPermissionsDesc(menu ={"物流管理","车辆管理"},button = "更新")
+    @RequestMapping("/updateTruck")
+    @ResponseBody
+    public Object updateTruck(HttpServletRequest request,
+                              @RequestParam(value = "companyId") long comanpyId,
+                              @RequestParam(value = "id") long id,
+                              @RequestParam(value = "licensePlateNumber") String licensePlateNumber,
+                              @RequestParam(value = "driver") String driver,
+                              @RequestParam(value = "phone") String phone,
+                              @RequestParam(value = "load") String load){
         return null;
     }
 
-    public Object delTruck(){
+    /*逻辑删除*/
+    @RequiresPermissions("admin:logistics:deltruck")
+    @RequiresPermissionsDesc(menu ={"物流管理","车辆管理"},button = "删除")
+    @RequestMapping("/delTruck")
+    @ResponseBody
+    public Object delTruck(HttpServletRequest request,
+                           @RequestParam(value = "companyId") long comanpyId,
+                           @RequestParam(value = "id") long id,
+                           @RequestParam(value = "licensePlateNumber") String licensePlateNumber,
+                           @RequestParam(value = "driver") String driver){
         return null;
     }
 
-    public Object truckList(){
+    @RequiresPermissions("admin:logistics:listtruck")
+    @RequiresPermissionsDesc(menu ={"物流管理","车辆管理"},button = "详情")
+    @RequestMapping("/listTruck")
+    @ResponseBody
+    public Object truckList(HttpServletRequest request,
+                            @RequestParam(value = "companyId" ,required = false) long comanpyId,
+                            @RequestParam(value = "id" ,required = false) long id,
+                            @RequestParam(value = "licensePlateNumber", required = false) String licensePlateNumber,
+                            @RequestParam(value = "driver",required = false) String driver){
         return null;
     }
 
-    
+
     /*物流订单管理 订单生成、订单状态更新、物流定单状态*/
+    @RequiresPermissions("admin:logistics:createOrder")
+    @RequiresPermissionsDesc(menu = {"物流管理","物流配送"},button = "创建")
+    @ResponseBody
+    @RequestMapping("/createOrder")
+    public Object createLogisticsOrder(HttpServletRequest request,
+                                       @RequestParam (value = "orders") String orders,
+                                       @RequestParam(value = "transitId") String transitId,
+                                       @RequestParam(value="companyId") long companyId,
+                                       @RequestParam(value = "ThirdOrder",required = false) String ThirdOrder,
+                                       @RequestParam(value = "licensePlateNumber") String licensePlateNumber,
+                                       @RequestParam(value = "freight",defaultValue = "0") int freight){
+        return null;
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/updateOrderStatus")
+    public Object updateOrderStatus(HttpServletRequest request,
+                                    @RequestParam(value = "orderId") String orderId,
+                                    @RequestParam(value = "status") int status){
+        return null;
+    }
+
+    @ResponseBody
+    @RequestMapping("/updateLogisticsOrderStatus")
+    public Object updateLogisticsOrderStatus(HttpServletRequest request,
+                                             @RequestParam(value = "transitId") String transitId,
+                                             @RequestParam(value = "status") int status){
+        return null;
+    }
+
+    @RequiresPermissions("admin:logistics:createOrder")
+    @RequiresPermissionsDesc(menu = {"物流管理","物流进度"},button = "查询")
+    @ResponseBody
+    @RequestMapping("/queryOrder")
+    public Object logisticsOrderList(HttpServletRequest request,
+                                     @RequestParam(value = "order", required = false) String order,
+                                     @RequestParam(value = "transitId", required = false) String transitId,
+                                     @RequestParam(value = "companyId", required = false) long companyId,
+                                     @RequestParam(value = "ThirdOrder", required = false) String ThirdOrder,
+                                     @RequestParam(value = "licensePlateNumber", required = false) String licensePlateNumber){
+        return null;
+    }
 }
